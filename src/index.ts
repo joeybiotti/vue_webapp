2dfdd0cd7a868878db764a044d2ad49b0f5e2fa7
Joey Biotti <joey@MacBook-Pro-2.local>
2017-11-05 13:56:38 -0600

    added vue components

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import Vue from "vue";
-import HelloComponent from "./components/Hello"
+import HelloComponent from "./components/Hello.vue";
 
 let v = new Vue({
     el: "#app",
@@ -9,7 +9,7 @@ let v = new Vue({
         <hello-component :name="name" :initialEnthusiasm="5" />
     </div>
     `,
-    data: {name: "World"},
+    data: { name: "World" },
     components: {
         HelloComponent
     }
